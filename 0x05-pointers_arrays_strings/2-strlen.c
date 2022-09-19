b9222471b33d0de8f3962b35e847e1116ac4aea9
chukwu1455 <tuk2chuks@gmail.com>
2022-09-19 15:53:01 -0700

    a function that prints the length of a street

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - length of string
+ *
+ * @s: string to get length
+ * Return: length number with nullterminator
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[1] != '\0'; i++)
+	{
+	}
+	return (i);
+}
