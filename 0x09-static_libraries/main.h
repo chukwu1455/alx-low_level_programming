85276ded2bbb1734f707001249b7d0beb0b8a946
chukwu1455 <tuk2chuks@gmail.com>
2022-09-30 07:46:58 -0700

    header function

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -7,7 +7,7 @@
  * and the prototype of the function _putchar
  */
 int _putchar(char c);
-int _islower(int c);int _isalpha(int c);
+int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
 int _isupper(int c);
