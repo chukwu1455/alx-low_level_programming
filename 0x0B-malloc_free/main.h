ae4c0e48ea8c70a1bc0f659b0015a7b2f719da5e
chukwu1455 <tuk2chuks@gmail.com>
2022-10-03 13:58:25 -0700

    funtion prototypes

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -7,7 +7,6 @@
  * and the prototype of the function _putchar
  */
 
-int _putchar(char c)
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
