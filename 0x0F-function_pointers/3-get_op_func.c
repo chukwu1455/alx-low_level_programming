4fa5b7e6b9691ec578d183deed173b451e7039bf
chukwu1455 <tuk2chuks@gmail.com>
2022-10-12 21:51:09 -0700

    simple operations

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,6 @@
 #include "3-calc.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * get_op_func - select the correct operation function asked by user
@@ -18,12 +19,12 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (i < 5)
+	while (ops[i].op)
 	{
-		if (strcmp(s, ops[i].op) == 0)
+		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
 
 		i++;
 	}
-	return (0);
+	return (NULL);
 }
