55889085c28c784127550c62e1657f0fe94dc244
chukwu1455 <tuk2chuks@gmail.com>
2022-10-20 08:14:49 -0700

    singly linked lists task completed

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,17 +9,16 @@
  */
 size_t print_list(const list_t *h)
 {
-	int nelem;
+	int count = 0;
 
-	nelem = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
+			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		nelem++;
+		count++;
 	}
-	return (nelem);
+	return (count);
 }
